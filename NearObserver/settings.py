04de6eab9a40dc672b6ebfc53ce90ac7071a9b6a
NB@mrdexstor <nb-zenfen04@mail.ru>
2025-02-06 22:59:54 +0300

    Init Commit

--- NearObserver/settings.py
+++ NearObserver/settings.py
@@ -125,6 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
