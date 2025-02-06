c69ea012913b8cacee34d6752900c24ab538de15
NB@mrdexstor <nb-zenfen04@mail.ru>
2025-02-06 22:53:42 +0300

    Init Commit

--- NearObserver/celery.py
+++ NearObserver/celery.py
@@ -5,3 +5,4 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NearObserver.settings')
 app = Celery('django')
 app.config_from_object('django.conf:settings', namespace='CALERY')
+app.autodiscover_tasks()
